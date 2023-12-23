9b0e4932dc1d2d7cf2248af0a3b9c37220ee13e8
Faraaz Jan <faraaz.jan@gmail.com>
2023-12-22 17:27:20 -0800

    Fixed text adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
         </div>
       </a>
       <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]" >
-        <p className="text-white text-9xl text-center font-montserrat">
+        <p className="text-white text-6xl sm:text-7xl md:text-8xl lg:text-9xl text-center font-montserrat">
           Coming Soon
         </p>
       </div>
